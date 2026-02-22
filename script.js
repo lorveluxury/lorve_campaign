aca197f16002215318e6f7e4210f9f4fb61eb6cd
lorveluxury <lorve.luxury1@gmail.com>
2026-02-22 11:15:45 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Lorvé live.");
+// Active link highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
